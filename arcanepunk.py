958dd7c61a90da625c9892ba561e8fd3df9bbb6c
Thunderborne <86179332+Thunderborne@users.noreply.github.com>
2024-04-29 00:01:01 -0700

    Add files via upload
    File organization uploaded

---
+++ arcanepunk.py
@@ -0,0 +1,15 @@
+# Author: Alfred Nguyen
+# Date: 04/27/2024
+# Version: 0.0
+# Course: CS 406 - Professor Van Londen
+# Description: Arcane Punk is a text-rpg where the user go through a
+# series of obstacles set in a futuristic-fantasy world. This file
+# houses the infinite loop that runs the main program.
+
+# Import functions
+
+# Ascii Art
+
+# Main Loop
+
+
---
+++ arcanepunk_data.py
@@ -0,0 +1,20 @@
+# Author: Alfred Nguyen
+# Date: 04/27/2024
+# Version: 0.0
+# Course: CS 406 - Professor Van Londen
+# Description: Arcane Punk is a text-rpg where the user go through a
+# series of obstacles set in a futuristic-fantasy world. This file
+# houses the microservice that will be used to check data at the
+# request of the main program (arcanepunk.py). Data is shared between
+# two text files (data_check.txt) and (checker.txt).
+
+# Import functions
+# Import time() function to delay read and write
+
+# Read file
+
+# Write file
+
+# Function that retrieves a data set
+
+# Infinite loop to check for data requests
